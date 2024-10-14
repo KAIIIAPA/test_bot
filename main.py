dae524152fcf15859aaaeaa19ce3e652d0559eef
Ildar <kaiiiapa1613@gmail.com>
2024-10-14 08:50:31 +0300

    v.1.2

--- main.py
+++ main.py
@@ -30,14 +30,14 @@ async def inform(message):
 async def inform_weather_message(message):
     city_name = message.text
     url = requests.get(
-            f"http://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={API_key}", timeout=1240
+            f"http://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={API_key}", timeout=240
         )
     weather_data = url.json()
-    temp = round((weather_data['main']['temp']-273.15), 2)
-    description = weather_data['weather'][0]['description']
-    translator = Translator(from_lang='English', to_lang='Russian')
-    description_tr = translator.translate(description)
-    if temp is not None:
+    if weather_data['cod'] == 200:
+        temp = round((weather_data['main']['temp']-273.15), 2)
+        description = weather_data['weather'][0]['description']
+        translator = Translator(from_lang='English', to_lang='Russian')
+        description_tr = translator.translate(description)
         await message.answer(f"Сейчас в городе {city_name}: {description_tr}. Температура: {temp} °C")
     else:
         await message.answer("Не удалось найти данные о погоде для этого города. Попробуйте другой.")
